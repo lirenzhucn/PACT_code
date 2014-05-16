6567c96fca97e208180ea3eb5c6a0f6fdc12419a
Liren Zhu <liren.zhu.cn@gmail.com>
2014-05-16 08:21:30 -0500

    Combining setup scripts into one.

--- setup_ext.py
+++ setup_ext.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
-"""Set up script for compiling the recon_loop package"""
+"""Combined setup script for all the extensions"""
 
 from distutils.core import setup, Extension
+from Cython.Build import cythonize
 import numpy
 
 # define the extension module
@@ -9,5 +10,9 @@ RECON_LOOP_MODULE = Extension('recon_loop',
                               sources=['recon_loop_ext.c'],
                               include_dirs=[numpy.get_include()])
 
+UNPACK_SPEEDUP_MODULE = Extension('unpack_speedup', ['unpack_speedup.pyx'],
+                                  include_dirs=[numpy.get_include()])
+
 # run the setup
 setup(ext_modules=[RECON_LOOP_MODULE])
+setup(ext_modules=cythonize(UNPACK_SPEEDUP_MODULE))
