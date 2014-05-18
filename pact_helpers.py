7cee942a26cbf8a570fe22f877d51d2b0804a68a
Liren Zhu <liren.zhu.cn@gmail.com>
2014-05-18 12:16:15 -0500

    Automatically print 'Done' at the end in updateProgress function.

--- pact_helpers.py
+++ pact_helpers.py
@@ -2,7 +2,7 @@ import yaml
 import sys
 
 def notifyCli(msg):
-    print msg
+  print msg
 
 def update_progress(current, total):
   """update progress bar"""
@@ -13,10 +13,12 @@ def update_progress(current, total):
   sys.stdout.write('\r{:>3}% [{:<50}]'.format(progress,\
                                               CHAR_INDICATOR*numIndicator))
   sys.stdout.flush()
+  if current == total:
+    print '\tDone'
 
 def loadOptions(opts_path):
-    # read and process YAML file
-    f = open(opts_path)
-    opts = yaml.safe_load(f)
-    f.close()
-    return opts
+  # read and process YAML file
+  f = open(opts_path)
+  opts = yaml.safe_load(f)
+  f.close()
+  return opts
