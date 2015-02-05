45e0cf59bbbeaa4cfff571d74c5fd1dd10878c1b
Liren Zhu <liren.zhu.cn@gmail.com>
2015-02-04 20:42:03 -0600

    Updated 3D reconstruction to a faster approach

--- reconstruct_3d_kernel.cu
+++ reconstruct_3d_kernel.cu
@@ -10,24 +10,76 @@ __global__ void init_image_kernel(float *img) {
   img[imgIdx] = 0.0;
 }
 
+__global__ void calculate_cos_alpha
+(float *cosAlpha, float *xRange, float *yRange,
+ float *xReceive, float *yReceive) {
+  size_t xi = blockIdx.x;
+  size_t yi = blockIdx.y;
+  size_t ni = threadIdx.x;
+  size_t idx = ni + yi*blockDim.x + xi*blockDim.x*gridDim.y;
+  float dx = xRange[xi] - xReceive[ni];
+  float dy = yRange[yi] - yReceive[ni];
+  float r0 = sqrt(xReceive[ni]*xReceive[ni] + yReceive[ni]*yReceive[ni]);
+  float rr0 = sqrt(dx*dx + dy*dy);
+  cosAlpha[idx] = fabs((-xReceive[ni]*dx-yReceive[ni]*dy)/r0/rr0);
+}
+
+__global__ void calculate_cos_alpha_and_tempc
+(float *cosAlpha, float *tempc, float *xRange, float *yRange,
+ float *xReceive, float *yReceive, float lenR) {
+  size_t xi = blockIdx.x;
+  size_t yi = blockIdx.y;
+  size_t ni = threadIdx.x;
+  size_t idx = ni + yi*blockDim.x + xi*blockDim.x*gridDim.y;
+  float dx = xRange[xi] - xReceive[ni];
+  float dy = yRange[yi] - yReceive[ni];
+  float r0 = sqrt(xReceive[ni]*xReceive[ni] + yReceive[ni]*yReceive[ni]);
+  float rr0 = sqrt(dx*dx + dy*dy);
+  cosAlpha[idx] = fabs((-xReceive[ni]*dx-yReceive[ni]*dy)/r0/rr0);
+  tempc[idx] = rr0 - lenR/cosAlpha[idx];
+}
+
 __global__ void backprojection_kernel
 (float *img, float *paDataLine,
- float *xRange, float *yRange, float *zRange,
+ float *cosAlpha_, float *xRange, float *yRange, float *zRange,
  float xReceive, float yReceive, float zReceive,
  float lenR, float vm, float delayIdx, float fs,
- unsigned int lineLength) {
+ unsigned int ni, unsigned int nSteps, unsigned int lineLength) {
   size_t xi = blockIdx.x;
   size_t yi = blockIdx.y;
   size_t zi = threadIdx.x;
   size_t imgIdx = zi + yi*blockDim.x + xi*blockDim.x*gridDim.y;
+  size_t precompIdx = ni + yi*nSteps + xi*nSteps*gridDim.y;
   float dx = xRange[xi] - xReceive;
   float dy = yRange[yi] - yReceive;
   float dz = zRange[zi] - zReceive;
-  float r0 = sqrt(xReceive*xReceive + yReceive*yReceive);
+  float cosAlpha = cosAlpha_[precompIdx];
   float rr0 = sqrt(dx*dx + dy*dy);
-  float cosAlpha = fabs((-xReceive*dx-yReceive*dy)/r0/rr0);
   float tempc = rr0 - lenR/cosAlpha;
   rr0 = sqrt(tempc*tempc + dz*dz)*SIGN(tempc) + lenR/cosAlpha;
+  if (fabs(dz/tempc) < fabs(10.0/lenR/cosAlpha)) {
+    float angleWeightB = tempc/sqrt(tempc*tempc+dz*dz)*cosAlpha/(rr0*rr0);
+    size_t idx0 = lround((rr0/vm-delayIdx)*fs);
+    if (idx0 < lineLength) {
+      img[imgIdx] += paDataLine[idx0] / angleWeightB;
+    }
+  }
+}
+
+__global__ void backprojection_kernel_fast
+(float *img, float *paDataLine,
+ float *cosAlpha_, float *tempc_, float *zRange,
+ float zReceive, float lenR, float vm, float delayIdx, float fs,
+ unsigned int ni, unsigned int nSteps, unsigned int lineLength) {
+  size_t xi = blockIdx.x;
+  size_t yi = blockIdx.y;
+  size_t zi = threadIdx.x;
+  size_t imgIdx = zi + yi*blockDim.x + xi*blockDim.x*gridDim.y;
+  size_t precompIdx = ni + yi*nSteps + xi*nSteps*gridDim.y;
+  float dz = zRange[zi] - zReceive;
+  float cosAlpha = cosAlpha_[precompIdx];
+  float tempc = tempc_[precompIdx];
+  float rr0 = sqrt(tempc*tempc + dz*dz)*SIGN(tempc) + lenR/cosAlpha;
   if (fabs(dz/tempc) < fabs(10.0/lenR/cosAlpha)) {
     float angleWeightB = tempc/sqrt(tempc*tempc+dz*dz)*cosAlpha/(rr0*rr0);
     size_t idx0 = lround((rr0/vm-delayIdx)*fs);
--- reconstruct_unpacked_3d.py
+++ reconstruct_unpacked_3d.py
@@ -4,26 +4,21 @@ import os
 import argh
 import numpy as np
 from time import time
-from pact_helpers import *
+from pact_helpers import notifyCli, loadOptions
+from pact_helpers import find_delay_idx, load_hdf5_data
+from pact_helpers import save_reconstructed_image
+from pact_helpers import update_progress_with_time
 from preprocess import subfunc_wiener, subfunc_exact
 
-#import pycuda.autoinit
 import pycuda.driver as cuda
 from pycuda.compiler import SourceModule
-from scipy.signal import hilbert
 from StringIO import StringIO
 
 KERNEL_CU_FILE = 'reconstruct_3d_kernel.cu'
-PULSE_STR = """
-129 133 137 141 145 149 153 157 161 165 169 173 177 181 185 189 385 389 393 397 401 405 409 413 417 421 425 429 433 437 441 445 257 261 265 269 273 277 281 285 289 293 297 301 305 309 313 317 1 5 9 13 17 21 25 29 33 37 41 45 49 53 57 61
-130 134 138 142 146 150 154 158 162 166 170 174 178 182 186 190 386 390 394 398 402 406 410 414 418 422 426 430 434 438 442 446 258 262 266 270 274 278 282 286 290 294 298 302 306 310 314 318 2 6 10 14 18 22 26 30 34 38 42 46 50 54 58 62
-131 135 139 143 147 151 155 159 163 167 171 175 179 183 187 191 387 391 395 399 403 407 411 415 419 423 427 431 435 439 443 447 259 263 267 271 275 279 283 287 291 295 299 303 307 311 315 319 3 7 11 15 19 23 27 31 35 39 43 47 51 55 59 63
-132 136 140 144 148 152 156 160 164 168 172 176 180 184 188 192 388 392 396 400 404 408 412 416 420 424 428 432 436 440 444 448 260 264 268 272 276 280 284 288 292 296 300 304 308 312 316 320 4 8 12 16 20 24 28 32 36 40 44 48 52 56 60 64
-196 200 204 208 212 216 220 224 228 232 236 240 244 248 252 256 452 456 460 464 468 472 476 480 484 488 492 496 500 504 508 512 324 328 332 336 340 344 348 352 356 360 364 368 372 376 380 384 68 72 76 80 84 88 92 96 100 104 108 112 116 120 124 128
-195 199 203 207 211 215 219 223 227 231 235 239 243 247 251 255 451 455 459 463 467 471 475 479 483 487 491 495 499 503 507 511 323 327 331 335 339 343 347 351 355 359 363 367 371 375 379 383 67 71 75 79 83 87 91 95 99 103 107 111 115 119 123 127
-194 198 202 206 210 214 218 222 226 230 234 238 242 246 250 254 450 454 458 462 466 470 474 478 482 486 490 494 498 502 506 510 322 326 330 334 338 342 346 350 354 358 362 366 370 374 378 382 66 70 74 78 82 86 90 94 98 102 106 110 114 118 122 126
-193 197 201 205 209 213 217 221 225 229 233 237 241 245 249 253 449 453 457 461 465 469 473 477 481 485 489 493 497 501 505 509 321 325 329 333 337 341 345 349 353 357 361 365 369 373 377 381 65 69 73 77 81 85 89 93 97 101 105 109 113 117 121 125
-"""
+PULSE_STR = ""
+PULSE_FILE = 'PULSE_ARRANGEMENTS.txt'
+with open(PULSE_FILE) as fid:
+    PULSE_STR = fid.read()
 
 
 def rearrange_pa_data(paData):
@@ -119,7 +114,6 @@ def reconstruction_3d_stational(paData, reconOpts,
         et = time()
         # use the execution time of the last loop to guess the remaining time
         time_remaining = ((zSteps - zi + 1) * (et - st_all) / (zi + 1)) / 60.0
-        #update_progress_with_time(zi+1, zSteps, time_remaining)
         progress(zi + 1, zSteps, time_remaining)
     cuda.memcpy_dtoh(reImg, d_reImg)
     et_all = time()
@@ -128,7 +122,8 @@ def reconstruction_3d_stational(paData, reconOpts,
     return reImg
 
 
-def reconstruction_3d(paData, reconOpts, progress=update_progress_with_time):
+def reconstruction_3d(paData, reconOpts, ctx,
+                      progress=update_progress_with_time):
     """3D reconstruction algorithm
     see Jun's focal-line reconstruction paper and codes for details
     """
@@ -146,7 +141,6 @@ def reconstruction_3d(paData, reconOpts, progress=update_progress_with_time):
     rf = reconOpts['resolution_factor']
     zrf = reconOpts['z_resolution']
     fs = reconOpts['fs']
-    algorithm = reconOpts['algorithm']
     # use the first z step data to calibrate DAQ delay
     delayIdx = find_delay_idx(paData[:, :, 0], fs)
     delayIdx = delayIdx.astype(np.float32)
@@ -159,13 +153,6 @@ def reconstruction_3d(paData, reconOpts, progress=update_progress_with_time):
     # notice the z step size is divided by firing group count
     zPerStep = zPerStep / numGroup
     zCenter = zPerStep * zSteps / 2
-    if algorithm == 'envelope':
-        notifyCli('Extracting envelope of A-line signals')
-        paData = np.abs(hilbert(paData, axis=0))
-        paData = paData.astype(np.float32)
-    elif algorithm == 'exact':
-        notifyCli('Filtered backprojection')
-        # TODO: impelement the 'exact' algorithm
     # notice nSteps is now 64!!
     anglePerStep = 2 * np.pi / nSteps / numGroup
     nPixelx = int(xSize * rf)
@@ -186,9 +173,13 @@ def reconstruction_3d(paData, reconOpts, progress=update_progress_with_time):
     yReceive = np.sin(detectorAngle) * R
     zReceive = np.arange(0, zSteps, dtype=np.float32) * zPerStep
     # create buffer on GPU for reconstructed image
-    reImg = np.zeros((nPixely, nPixelx, nPixelz), order='C', dtype=np.float32)
+    reImg = np.zeros((nPixely, nPixelx, nPixelz),
+                     order='C', dtype=np.float32)
     d_reImg = cuda.mem_alloc(nPixely * nPixelx * nPixelz * 4)
     cuda.memcpy_htod(d_reImg, reImg)
+    d_cosAlpha = cuda.mem_alloc(nPixely * nPixelx * nSteps * numGroup * 4)
+    d_tempc = cuda.mem_alloc(nPixely * nPixelx * nSteps * numGroup * 4)
+    d_paDataLine = cuda.mem_alloc(nSamples * 4)
     # back projection loop
     notifyCli('Reconstruction starting. Keep patient.')
     d_xRange = cuda.mem_alloc(xRange.nbytes)
@@ -197,28 +188,38 @@ def reconstruction_3d(paData, reconOpts, progress=update_progress_with_time):
     cuda.memcpy_htod(d_yRange, yRange)
     d_zRange = cuda.mem_alloc(zRange.nbytes)
     cuda.memcpy_htod(d_zRange, zRange)
+    d_xReceive = cuda.mem_alloc(xReceive.nbytes)
+    cuda.memcpy_htod(d_xReceive, xReceive)
+    d_yReceive = cuda.mem_alloc(yReceive.nbytes)
+    cuda.memcpy_htod(d_yReceive, yReceive)
     # get module right before execution of function
     MOD = SourceModule(open(KERNEL_CU_FILE, 'r').read())
-    bpk = MOD.get_function('backprojection_kernel')
-    d_paDataLine = cuda.mem_alloc(nSamples * 4)
+    precomp = MOD.get_function('calculate_cos_alpha_and_tempc')
+    bpk = MOD.get_function('backprojection_kernel_fast')
+    # compute cosAlpha and tempc
     st_all = time()
+    precomp(d_cosAlpha, d_tempc, d_xRange, d_yRange,
+            d_xReceive, d_yReceive, np.float32(lenR),
+            grid=(nPixelx, nPixely), block=(nSteps * numGroup, 1, 1))
+    ctx.synchronize()
+    notifyCli('Done pre-computing cosAlpha and tempc.')
+    st = time()
     for zi in range(zSteps):
-        st = time()
         # find out the index of fire at each virtual plane
         fi = zi % numGroup
         for ni in range(nSteps):
             # transducer index
             ti = pulseList[fi, ni]
             cuda.memcpy_htod(d_paDataLine, paData[:, ni, zi])
-            bpk(d_reImg, d_paDataLine, d_xRange, d_yRange, d_zRange,
-                xReceive[ti], yReceive[ti], zReceive[zi],
-                np.float32(lenR), np.float32(vm), delayIdx[ti],
-                np.float32(fs), np.uint32(nSamples),
+            bpk(d_reImg, d_paDataLine, d_cosAlpha, d_tempc,
+                d_zRange, zReceive[zi], np.float32(lenR),
+                np.float32(vm), delayIdx[ti], np.float32(fs),
+                np.uint32(ti), np.uint32(nSteps * numGroup),
+                np.uint32(nSamples),
                 grid=(nPixelx, nPixely), block=(nPixelz, 1, 1))
         et = time()
         # use the execution time of the last loop to guess the remaining time
-        time_remaining = ((zSteps - zi) * (et - st)) / 60.0
-        #update_progress_with_time(zi+1, zSteps, time_remaining)
+        time_remaining = ((zSteps - zi - 1) * (et - st) / (zi + 1)) / 60.0
         progress(zi + 1, zSteps, time_remaining)
     cuda.memcpy_dtoh(reImg, d_reImg)
     et_all = time()
@@ -263,7 +264,7 @@ def reconstruct_3d(opts, progress=update_progress_with_time):
     cuda.init()
     dev = cuda.Device(0)
     ctx = dev.make_context()
-    reImg = reconstruction_3d(chn_data_3d, opts['recon'], progress)
+    reImg = reconstruction_3d(chn_data_3d, opts['recon'], ctx, progress)
     ctx.pop()
     del ctx
     save_reconstructed_image(reImg, opts['extra']['dest_dir'],
